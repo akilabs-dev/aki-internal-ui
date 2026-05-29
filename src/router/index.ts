3dcb98681e635c7a2142e9b10f340987eabad25e
Chiva SONG <chiva.song@akilabs.co>
2026-05-29 11:35:43 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -127,7 +127,7 @@ const routes: RouteRecordRaw[] = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior() {
     return { top: 0 }
